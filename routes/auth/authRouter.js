c251df2b5a06f418fa9d35dc013e5c7ecbcf9b4f
co9mltjs <co9mltjs@gmail.com>
2025-09-03 23:24:57 +0900

    fix: 로그인 오류 수정

--- routes/auth/authRouter.js
+++ routes/auth/authRouter.js
@@ -2,13 +2,13 @@ import express from "express";
 import passport from "passport";
 import { jwtStrategy, localStrategy } from "../../controllers/auth/authController.js";
 
-
 const authRouter = express.Router();
 
 // 이메일, 비밀번호 로그인 전략
-authRouter.post("/local", passport.authenticate('local', { session : false }), localStrategy)
+authRouter.post("/local", passport.authenticate("local", { session: false }), localStrategy);
+authRouter.get("/jwt", passport.authenticate("jwt", { session: false }), jwtStrategy);
 
 // jwt 토큰 전략
-authRouter.post("/jwt", passport.authenticate('jwt', {session: false}), jwtStrategy)
+authRouter.post("/jwt", passport.authenticate("jwt", { session: false }), jwtStrategy);
 
 export default authRouter;
